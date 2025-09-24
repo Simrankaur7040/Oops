d26c77b9b06257b83af3ed867b5e28f8a88f2cf9
Simrankaur7040 <simrankaur22370@gmail.com>
2025-09-24 20:35:18 +0530

    Add files via upload

---
+++ Inheritance/AnimalHierarchy.java
@@ -0,0 +1,63 @@
+// Superclass
+class Animal {
+    String name;
+    int age;
+
+    
+    Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println(name + " Dog: Woof Woof!");
+    }
+}
+
+
+class Cat extends Animal {
+    Cat(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println(name + " Cat: Meow Meow!");
+    }
+}
+
+class Bird extends Animal {
+    Bird(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println(name + " Bird: Tweet Tweet!");
+    }
+}
+
+public class AnimalHierarchy {
+    public static void main(String[] args) {
+        
+        Animal dog = new Dog("Tommy", 3);
+        Animal cat = new Cat("Kitty", 2);
+        Animal bird = new Bird("Tweety", 1);
+
+       
+        dog.makeSound();
+        cat.makeSound();
+        bird.makeSound();
+    }
+}
